38d124146dc037a48bd50a0db18ee5890a2ecc48
Michael Bossner <Michael.bossner@hotmail.com>
2019-07-03 17:31:44 +1000

    v1.0
    adding v1.0 of proc-reboot

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-unix-process-rebooter
+proc-reboot
+
+A python script for unix based systems that runs a list of processes and
+restarts them when they end for any reason.
+
+PROC - is the list of process Paths. Add any processes you wish to run
+       to this list.
+
+running the script with stdout piped into a log file is the ideal way
+to run. eg "$ python proc-reboot > log.txt 2>1&"
---
+++ proc-reboot.py
@@ -0,0 +1,49 @@
+# Author: Mirai-Miki
+# Discord Bot rebooter for Unix systems.
+# Restarts any bot that stops for any reason.
+
+from subprocess import Popen
+from time import sleep
+import os
+import signal
+import sys
+
+NODE = "node"
+# Add paths of Bots to PROC list
+PROC = ["/home/pi/programs/discord/dice/bot.js"]
+WAIT = 10
+pid = []
+
+################################# Functions ###################################
+
+def receiveSignal(signalNumber, frame):
+	print("Shutting Down")
+	for i in range(len(pid)):
+		os.killpg(os.getpgid(pid[i]), signal.SIGKILL)
+		print("Killed: "+str(pid[i]))
+		
+	print("Shutdown Completed - Goodbye")
+	exit()
+
+################################ Main #########################################
+
+if __name__ == '__main__':
+	signal.signal(signal.SIGINT, receiveSignal)
+	signal.signal(signal.SIGTERM, receiveSignal)
+	print("PID: "+str(os.getpid()))
+	sys.stdout.flush()
+
+	for i in range(len(PROC)):
+		pid.append(os.fork()) # Spin up new process
+		if (pid[i] == 0): # This is the rebooter process
+			os.setsid()
+			while (True): # Forever reboot on bots end
+				print("Booting: "+PROC[i])
+				sys.stdout.flush()
+				proc = Popen([NODE, PROC[i]])
+				status = proc.wait()
+				print((PROC[i]+" Exited with code: "+str(status)))
+				sys.stdout.flush()
+				sleep(WAIT)
+	# After all bots have been started on there forever loop wait for shutdown
+	signal.pause()
